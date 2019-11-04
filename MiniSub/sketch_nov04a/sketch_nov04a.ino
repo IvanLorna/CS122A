6210fb4463497178f5f4e06a0ccbbaf1ea5cd3f0
Ivan Lorna <ivanlorna1710@gmail.com>
2019-11-04 14:49:15 -0800

    working PWM and serial

--- MiniSub/sketch_nov04a/sketch_nov04a.ino
+++ MiniSub/sketch_nov04a/sketch_nov04a.ino
@@ -1,20 +1,33 @@
 IntervalTimer myTimer;
-
+int counter = 0;
+int LED = LOW;
+int PWM = 50;
 void setup() {
-  //pinMode(A0,OUTPUT);
-  //digitalWrite(A0,HIGH); 
-  serial.begin(9600);  //FIXME::add serial library
+  pinMode(A0,OUTPUT);
+  counter = 0;
+  PWM = 25;
+  LED = LOW; 
+  Serial.begin(9600);  //FIXME::add serial library
   //attachInterrupt(digitalPinToInterrupt(A1), Work, RISING);
-  myTimer.begin(Work, 1500000);
+  myTimer.begin(Work, 15000);
 }
 
 void Work() {
-  if (A0 == HIGH) {digitalWrite(A0,LOW);}
-  else { digitalWrite(A0, HIGH);}  
-  serial.print( " :^) ");
+  counter++;
+  if (counter >= 100) {
+    counter = 0;
+  }
+  else if (counter > PWM) {
+    LED = HIGH;
+  }
+  else if (counter < PWM) {
+    LED = LOW;
+  }
+  digitalWrite(A0,LED);  
+  Serial.print( " :^) ");
 }
 
 
 void loop() {
-  idle; //FIXME:: add idle/sleep library
+//  idle; //FIXME:: add idle/sleep library
 }
