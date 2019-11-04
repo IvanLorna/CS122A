63635b00b9f8e41079ddf53b6d0704a3ba26a575
Ivan Lorna <ivanlorna1710@gmail.com>
2019-11-04 13:30:36 -0800

    use IntervalTimer for shceduler

--- MiniSub/sketch_nov04a/sketch_nov04a.ino
+++ MiniSub/sketch_nov04a/sketch_nov04a.ino
@@ -1,13 +1,20 @@
+IntervalTimer myTimer;
 
 void setup() {
-  pinMode(A0,OUTPUT);
-  digitalWrite(A0,HIGH); 
+  //pinMode(A0,OUTPUT);
+  //digitalWrite(A0,HIGH); 
+  serial.begin(9600);  //FIXME::add serial library
+  //attachInterrupt(digitalPinToInterrupt(A1), Work, RISING);
+  myTimer.begin(Work, 1500000);
 }
 
-void loop() {
-  delay(25);
+void Work() {
   if (A0 == HIGH) {digitalWrite(A0,LOW);}
-  else { digitalWrite(A0, HIGH);}
-  
+  else { digitalWrite(A0, HIGH);}  
+  serial.print( " :^) ");
+}
 
+
+void loop() {
+  idle; //FIXME:: add idle/sleep library
 }
